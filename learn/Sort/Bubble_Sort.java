040aa031b3596e0e812f6985b3f41949edaa6a86
sthgreat <284387885@qq.com name = sthgreat>
2019-07-31 11:33:45 +0800

    add Bubble_Sort

--- learn/Sort/Bubble_Sort.java
+++ learn/Sort/Bubble_Sort.java
@@ -3,12 +3,12 @@ package Sort;
 public class Bubble_Sort {
     private void sort(int[] arr) {//原始冒泡排序
         int temp;
-        for (int i=0; i<arr.length;i++){
-            for(int j =0;j<arr.length-1;j++){
-                if(arr[j]>arr[j+1]){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
                     temp = arr[j];
-                    arr[j] =arr[i];
-                    arr[i] =temp;
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
         }
@@ -16,6 +16,9 @@ public class Bubble_Sort {
 
     public static void main(String[] args) {
         int[] a = {4, 2, 7, 5, 9, 12, 3, 8};
-
+        Bubble_Sort bubble_sort = new Bubble_Sort();
+        SortUtils sortUtils = new SortUtils();
+        bubble_sort.sort(a);
+        sortUtils.show(a);
     }
 }
