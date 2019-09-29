69e11682d6a5a1de60bbe99f0bbadaf98536aebc
sthgreat <284387885@qq.com>
2019-09-29 20:43:25 +0800

    修改11

--- LeetCode/Algorithm/Algorithm_11.java
+++ LeetCode/Algorithm/Algorithm_11.java
@@ -1,6 +1,12 @@
 package Algorithm;
 
 public class Algorithm_11 {
+    /**
+     * 思路：规定两个指针，从两侧开始，将最短的那个指针向中间移动，使用一个定值保存
+     * 面积大小，每一轮进行比较，留下最大的那个。
+     * 可行性：长度也是制约因素，开始取长度最长，记录下一个值，当长度缩小时，需要修改当前
+     * 最短的那条边，向中间移动，以求获得一个长边，面积大于最大的面积
+     */
     class Solution {
         public int maxArea(int[] height) {
             int max = 0;
