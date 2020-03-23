01799dbbc37bfcc0aba65e089ebc27ad803d1c4d
sthgreat <284387885@qq.com>
2020-03-23 21:22:36 +0800

    手撕红黑树

---
+++ learn/data_structure/MyRBTree.java
@@ -0,0 +1,12 @@
+package data_structure;
+
+public class MyRBTree {
+    MyRBTreeNode root;
+}
+
+class MyRBTreeNode{
+    boolean color;
+    MyRBTreeNode leftNode;
+    MyRBTreeNode rightNode;
+    Object value;
+}
