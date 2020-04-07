ec81ad2bfc7e32cd2101f729318734920af7af24
sthgreat <284387885@qq.com>
2020-04-07 22:24:59 +0800

    vivo2020-数位之积

--- learn/Niuke/VIVO_ShuWeiZhiJi.java
+++ learn/Niuke/VIVO_ShuWeiZhiJi.java
@@ -2,8 +2,11 @@ package Niuke;
 
 import java.util.*;
 
+/***
+ * 现给定任意正整数 n，请寻找并输出最小的正整数 m（m>9），使得 m 的各位（个位、十位、百位 ... ...）之乘积等于n，若不存在则输出 -1。
+ */
 
-public class test2 {
+public class VIVO_ShuWeiZhiJi {
     /**
      * 输入一个整形数值，返回一个整形值
      * @param n int整型 n>9
@@ -11,12 +14,6 @@ public class test2 {
      */
     static LinkedList<Integer> result = new LinkedList<>();
     static boolean flag = true;
-
-    public static void main(String[] args){
-        test2 t = new test2();
-        System.out.println(t.solution(100));
-    }
-
     public int solution (int n) {
         // write code here
         perm(n);
