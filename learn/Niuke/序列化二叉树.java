00c23cd68c9727e6fce690df3e5fba1b41366313
sthgreat <284387885@qq.com>
2020-04-12 20:26:25 +0800

    牛客网-序列化二叉树

--- learn/Niuke/序列化二叉树.java
+++ learn/Niuke/序列化二叉树.java
@@ -1,4 +1,4 @@
-package Base;/*
+package Niuke;/*
 public class TreeNode {
     int val = 0;
     TreeNode left = null;
@@ -12,25 +12,19 @@ public class TreeNode {
 }
 */
 import java.util.*;
-public class Test {
-    static StringBuilder pre = new StringBuilder();
-    static StringBuilder in = new StringBuilder();
 
-    public static void main(String[] args){
-        TreeNode node1 = new TreeNode(1);
-        TreeNode node2 = new TreeNode(2);
-        TreeNode node3 = new TreeNode(3);
-        node1.left = node2;
-        node1.right = node3;
-        Test t = new Test();
-        String serialize = t.Serialize(node1);
-        TreeNode deserialize = t.Deserialize(serialize);
-        System.out.println(deserialize.val);
-        System.out.println(deserialize.left.val);
-        System.out.println(deserialize.right.val);
-    }
+/***
+ * 注意“.”不能作为字符串分隔符？？
+ * 可参考根据中序前序遍历恢复二叉树，将二叉树序列化为中序和前序遍历节点顺序
+ * 也可采用层序遍历的方式，记录空节点和层序
+ */
+public class 序列化二叉树 {
+    static StringBuilder pre;
+    static StringBuilder in;
 
     String Serialize(TreeNode root) {
+        pre = new StringBuilder();
+        in = new StringBuilder();
         if(root == null){
             return "";
         }
@@ -62,7 +56,7 @@ public class Test {
                 mid = i;
             }
         }
-        TreeNode node = new TreeNode(Integer.parseInt(pre[0]));
+        TreeNode node = new TreeNode(Integer.valueOf(pre[0]));
 
         node.left = reBuild(Arrays.copyOfRange(pre, 1, mid + 1),Arrays.copyOfRange(in, 0, mid));
         node.right = reBuild(Arrays.copyOfRange(pre, mid + 1, pre.length), Arrays.copyOfRange(in, mid + 1,in.length));
@@ -89,16 +83,4 @@ public class Test {
             inOrder(root.right);
         }
     }
-}
-
-class TreeNode {
-    int val = 0;
-    TreeNode left = null;
-    TreeNode right = null;
-
-    public TreeNode(int val) {
-        this.val = val;
-
-    }
-
 }
