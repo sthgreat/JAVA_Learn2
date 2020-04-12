eafbd628ebb286eb69af676e153bde4e844799f5
sthgreat <284387885@qq.com>
2020-04-12 22:04:37 +0800

    牛客网-滑动窗口的最大值

--- learn/Niuke/滑动窗口的最大值.java
+++ learn/Niuke/滑动窗口的最大值.java
@@ -1,26 +1,20 @@
-package Base;
+package Niuke;
 
 import java.util.*;
-public class Test {
+
+/***
+ * 需要注意：最大值需要更新
+ */
+public class 滑动窗口的最大值 {
     int max;
     int curMax;
 
-    public static void main(String[] args){
-        Test t = new Test();
-        int[] num = new int[]{2,3,4,2,6,2,5,1};
-        ArrayList<Integer> integers = t.maxInWindows(num, 3);
-        System.out.println();
-        for(int i : integers){
-            System.out.print(i+" ");
-        }
-    }
-
     public ArrayList<Integer> maxInWindows(int [] num, int size)
     {
-        if(num.length < size){
-            return null;
-        }
         ArrayList<Integer> result = new ArrayList<>();
+        if(num.length < size || size == 0){
+            return result ;
+        }
         max = num[0];
         curMax = 0;
         for(int i = 0;i<size;i++){
@@ -30,8 +24,7 @@ public class Test {
             }
         }
         for(int i = size - 1;i<num.length;i++){//判断最大值是否过期
-            System.out.print(curMax+" "+(i-size+1)+".");
-            if(curMax < (i - size + 1)){//最大值过期
+            if(curMax < i - size + 1){//最大值过期
                 findMax(num,i - size + 1, i);//更新最大值
                 result.add(max);
             }else{
