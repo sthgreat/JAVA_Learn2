ccc3febbdbf5b5095e20b6dd1d489eb1450211f3
sthgreat <284387885@qq.com>
2020-04-15 22:44:17 +0800

    leetcode-快速幂-50

---
+++ learn/LeetCode/Pow_50.java
@@ -0,0 +1,26 @@
+package LeetCode;
+
+public class Pow_50 {
+    public double myPow(double x, int n) {
+        if(n<0){
+            x = 1/x;
+            n = -n;
+        }
+        if(n == 0){
+            return 1;
+        }
+        return pow(x,n);
+    }
+
+    public double pow(double x, int n){
+        if(n == 0){
+            return 1.0;
+        }
+        double half = pow(x, n/2);
+        if(n%2 == 0){
+            return half*half;
+        }else{
+            return half*half*x;
+        }
+    }
+}
--- learn/LeetCode/Test.java
+++ learn/LeetCode/Test.java
@@ -6,10 +6,7 @@ import java.util.HashMap;
 
 public class Test {
     public static void main(String[] args){
-        HashMap<String, String> map = new HashMap<String, String>();
-        String s = map.get("1");
-        System.out.println(s);
-        Collection<String> values = map.values();
-        new ArrayList<>();
+        double x = 1.1;
+        System.out.println(1/x);
     }
 }
