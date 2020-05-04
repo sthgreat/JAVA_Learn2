28c1d03a4f2a71d059648b1fd6062cbf96bcee30
sthgreat <284387885@qq.com>
2020-05-04 16:33:07 +0800

    leetcode-对称的二叉树-101

---
+++ learn/LeetCode/对称的二叉树_101.java
@@ -0,0 +1,18 @@
+package LeetCode;
+
+public class 对称的二叉树_101 {
+    public boolean isSymmetric(TreeNode root) {
+        return isMirror(root, root);
+    }
+
+    boolean isMirror(TreeNode rootLeft, TreeNode rootRight){
+        if(rootLeft == null && rootRight == null){
+            return true;
+        }
+        if(rootLeft == null || rootRight == null){
+            return false;
+        }
+        return rootLeft.val == rootRight.val && isMirror(rootLeft.left, rootRight.right)
+                && isMirror(rootLeft.right, rootRight.left);
+    }
+}
