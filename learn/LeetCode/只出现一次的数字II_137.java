1ce8a4cfd60e213485f1ef7b245aad791144e6dd
sthgreat <284387885@qq.com>
2020-05-15 10:07:25 +0800

    leetcode-137-只出现一次的数字II

---
+++ learn/LeetCode/只出现一次的数字II_137.java
@@ -0,0 +1,29 @@
+package LeetCode;
+
+/***
+ * 利用位运算实现常数级别的空间复杂度，不太看得懂
+ */
+public class 只出现一次的数字II_137 {
+    public int singleNumber(int[] nums) {
+        int seenOnce = 0, seenTwice = 0;
+
+        for (int num : nums) {
+            // first appearence:
+            // add num to seen_once
+            // don't add to seen_twice because of presence in seen_once
+
+            // second appearance:
+            // remove num from seen_once
+            // add num to seen_twice
+
+            // third appearance:
+            // don't add to seen_once because of presence in seen_twice
+            // remove num from seen_twice
+            seenOnce = ~seenTwice & (seenOnce ^ num);
+            seenTwice = ~seenOnce & (seenTwice ^ num);
+        }
+
+        return seenOnce;
+    }
+}
+
--- learn/Test/test.java
+++ learn/Test/test.java
@@ -4,50 +4,13 @@ package Test;
 public class test {
     int count = 0;
     int result = 0;
-    public int TreeDepth(TreeNode root) {
-        if(root == null){
-            return 0;
-        }
-        dfs(root);
-        return result;
-    }
-
-    private void dfs(TreeNode node){
-        count += 1;
-        result = Math.max(count, result);
-        if(node.left!=null){
-            dfs(node.left);
-            count -= 1;
-        }
-        if(node.right!=null){
-            dfs(node.right);
-            count -= 1;
-        }
+    static void add(Integer i){
+        i = 100;
     }
 
     public static void main(String[] args){
-        TreeNode n1 = new TreeNode(1);
-        TreeNode n2 = new TreeNode(2);
-        TreeNode n3 = new TreeNode(3);
-        TreeNode n4 = new TreeNode(4);
-        TreeNode n5 = new TreeNode(5);
-        TreeNode n6 = new TreeNode(6);
-        TreeNode n7 = new TreeNode(7);
-        n1.left = n2;
-        n1.right = n3;
-        n2.left = n4;
-        n2.right = n5;
-        n3.right = n6;
-        n5.left = n7;
-        test t = new test();
-        System.out.println(t.TreeDepth(n1));
-    }
-}
-class TreeNode {
-    int val = 0;
-    TreeNode left = null;
-    TreeNode right = null;
-    public TreeNode(int val) {
-        this.val = val;
+        Integer i = 9999;
+        add(i);
+        System.out.println(i);
     }
 }
