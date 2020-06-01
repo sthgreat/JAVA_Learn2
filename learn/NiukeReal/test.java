f41fcefdcd5333def43c1415aa249e28a41b8f69
sthgreat <284387885@qq.com>
2020-06-01 15:36:25 +0800

    leetcode-删除链表中的节点

--- learn/NiukeReal/test.java
+++ learn/NiukeReal/test.java
@@ -1,12 +1,30 @@
 package NiukeReal;
 
-import java.util.Scanner;
+import java.nio.channels.ServerSocketChannel;
+import java.util.*;
 import java.util.concurrent.Future;
 
 public class test {
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        String s = sc.next();
-        System.out.println(Math.pow(2, 1/2));
+        Queue<Tow> q = new PriorityQueue<>(new Comparator<Tow>() {
+            @Override
+            public int compare(Tow o1, Tow o2) {
+                return o1.num - o2.num;
+            }
+        });
+        q.add(new Tow(2));
+        q.add(new Tow(4));
+        q.add(new Tow(3));
+        System.out.println(q.poll().toString());
+    }
+}
+
+class Tow{
+    int num;
+    public Tow(int n){
+        this.num = n;
+    }
+    public String toString(){
+        return this.num + "";
     }
 }
