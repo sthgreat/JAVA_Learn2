ec66b015aed47c0e1170a2bc8be24a41b3ce7858
sthgreat <284387885@qq.com>
2020-06-05 16:34:22 +0800

    leetcode-6-Z字形变换

--- learn/LeetCode/Z字形变换_6.java
+++ learn/LeetCode/Z字形变换_6.java
@@ -1,27 +1,15 @@
 package LeetCode;
 
-import LeetCode.ListNode;
-
 import java.util.HashMap;
-import java.util.List;
-import java.util.PriorityQueue;
 
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
-public class test {
-    public static void main(String[] args){
-        test t = new test();
-        String s = t.convert("LEETCODEISHIRING", 3);
-        System.out.println(s);
-    }
-
+public class Z字形变换_6 {
     public String convert(String s, int numRows) {
+        if(s.length() == 0){
+            return "";
+        }
+        if(numRows == 1){
+            return s;
+        }
         int r = 1;
         HashMap<Integer, String> map = new HashMap<>();
         boolean flag = true; //true时递增，false时递减
@@ -39,6 +27,7 @@ public class test {
             }
         }
         String ans = "";
+        numRows = Math.min(s.length(), numRows);
         for(int i = 1;i<=numRows;i++){
             ans += map.get(i);
         }
@@ -46,7 +35,6 @@ public class test {
     }
 
     public static void add(HashMap<Integer, String> map, int cur, int h, String s){
-        System.out.println(h);
         if(!map.containsKey(h)){
             map.put(h, s.charAt(cur) + "");
         }else{
