9050fadd0f4cf7750e2aabdf0e25de12b142f29d
sthgreat <284387885@qq.com>
2020-06-17 16:57:36 +0800

    leetcode-位1的个数-191

---
+++ learn/LeetCode/位1的个数_191.java
@@ -0,0 +1,18 @@
+package LeetCode;
+
+/***
+ * 位运算
+ */
+public class 位1的个数_191 {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int ans = 0;
+        for(int i = 0;i<32;i++){
+            if((n & 1) == 1){
+                ans ++;
+            }
+            n = n >>> 1;
+        }
+        return ans;
+    }
+}
