06db205a83f9066cee90b22938a6a150f12ba477
sthgreat <284387885@qq.com>
2020-06-29 10:54:55 +0800

    leetcode-二叉树最近的公共祖先II

---
+++ learn/LeetCode/二叉树最近的公共祖先II.java
@@ -0,0 +1,39 @@
+package LeetCode;
+
+import LeetCode.TreeNode;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class 二叉树最近的公共祖先II {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == p){
+            return p;
+        }else if(root == q){
+            return q;
+        }else if(root == null){
+            return null;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if(left != null && right != null){ //找到了，返回自己
+            return root;
+        }else if(left == null && right == null){
+            return null;
+        }else{
+            if(left != null){
+                return left;
+            }else {
+                return right;
+            }
+        }
+    }
+}
