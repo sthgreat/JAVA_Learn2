69d4809bef997d3dc384a93a5c2d42f1264640a9
sthgreat <284387885@qq.com>
2020-07-16 20:02:36 +0800

    剑指Offer-14-剪绳子

---
+++ learn/SwordToOffer/剪绳子_14.java
@@ -0,0 +1,17 @@
+package SwordToOffer;
+
+public class 剪绳子_14 {
+    public int cuttingRope(int n) {
+        if(n <= 3){
+            return n-1;
+        }
+        int a = n / 3;
+        int b = n % 3;
+        if(b == 0){
+            return (int) Math.pow(3, a);
+        }else if(b == 1){
+            return (int) Math.pow(3, a - 1) * 4;
+        }
+        return (int) Math.pow(3, a)*2;
+    }
+}
